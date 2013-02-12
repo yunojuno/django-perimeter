98027dc8fcc18c8efe89e4c5b7d834c20b6e8746
Hugo Rodger-Brown <hugo@rodger-brown.com>
2013-02-12 17:31:00 +0000

    Now in distributable package format.
    I have changed the directory structure and added the packaging setup
    files, ready for deployment to PyPI. Have also removed migrations, as
    these are no longer required.

---
+++ setup.py
@@ -0,0 +1,31 @@
+import os
+from setuptools import setup
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-perimeter',
+    version='0.1',
+    packages=['django_perimeter'],
+    include_package_data=True,
+    license='BSD License',
+    description='Site-wide perimeter access control for Django projects.',
+    long_description=README,
+    url='https://github.com/hugorodgerbrown/django-perimeter',
+    author='Hugo Rodger-Brown',
+    author_email='hugo@yunojuno.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)

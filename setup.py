2c41da170bd09a15ae189298f85a7e92c3e77de8
Hugo Rodger-Brown <hugo@rodger-brown.com>
2015-08-06 17:12:34 +0100

    Bump version to 0.6.6

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-perimeter',
-    version='0.6.5',
+    version='0.6.6',
     packages=['perimeter', 'perimeter.tests', 'perimeter.management.commands'],
     include_package_data=True,
     license='BSD License',
